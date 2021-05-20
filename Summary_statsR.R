8f12287806c084623635b627cc0b2ca926a51d7c
mspector13 <mspector.ecology@gmail.com>
2021-05-20 13:29:25 -0700

    Added working community data sheetst

--- Summary_statsR.R
+++ Summary_statsR.R
@@ -5,7 +5,12 @@ library(gt)
 library(ggplot2)
 library(rstatix)
 
-#Production analysis/summary stats
+
+# #Production analysis/summary stats --------------------------------------
+
+
+
+
 
 PI_all <- read_xlsx("PI_all.xlsx") %>% 
   filter(Species != 'Acrosorium venulosum on Articulated Cor.')
@@ -65,4 +70,27 @@ PI_all %>%
 
 
 
+
+
+# Community reponses ------------------------------------------------------
+
+
+PL_comm <-  read_csv("./Community_data/Pl_data.csv") 
+
+no_zero <- PL_comm %>% 
+  # filter out sites with 0 observations
+  mutate(sum = rowSums(across(where(is.numeric)))) %>% 
+  filter(sum > 0) %>% 
+  select(-sum) 
+
+view(PL_comm)
+
+#make this same figure, but split Treatment so it's not stacked
+#add error bars
+#Figure out appropriate number of species to graph (n= ~4), 
+#rest go in table
+PL_comm %>% 
+  ggplot(aes(x = SEASON, y = ABUNDANCE, fill = TREATMENT)) +
+  geom_bar(stat='identity') +
+  facet_wrap(~SPECIES)
 
